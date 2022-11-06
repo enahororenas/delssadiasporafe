8ab0d38876af4610f741bd4fb877ff42769769a9
enahoroenas <orieroenahoro@yahoo.com>
2022-11-06 14:38:49 -0800

    added projects

---
+++ src/pages/dashboard/Project.js
@@ -0,0 +1,44 @@
+import React,{useEffect} from 'react'
+import Wrapper from '../../assets/wrappers/Projects'
+import {Navbar,BigSidebar,SmallSidebar,Projectitem} from '../../components'
+import { useAppContext} from '../../context/appContext'
+
+const Project = () => {
+const {getProjects,projects,totalProject} = useAppContext()
+  useEffect(()=>{
+    getProjects()
+   },[totalProject])
+
+   const Convert = (value) => {
+    return parseFloat(value)
+   }
+
+  return (
+    <Wrapper>
+    <div className='dashboard'>
+        <SmallSidebar/>
+        <BigSidebar/>
+            <div>
+                <Navbar top ='Chapter Projects'/>
+                <div className='proj'>
+                <ol>
+                    {projects.length !== 0 &&  projects.map((projItem, index) => {
+                    const lk = 'L'+index
+                    return  ( <li key={lk}>
+                      <Projectitem key={index} 
+                        total = {Convert(projItem.total)}
+                        completed={Convert(projItem.completed)}
+                        topic = {projItem.topic}
+                        unit = {projItem.unit}
+                        />
+                    </li>)})}
+                    
+                </ol>
+                </div>
+            </div>
+    </div>   
+    </Wrapper>     
+  )
+}
+
+export default Project
