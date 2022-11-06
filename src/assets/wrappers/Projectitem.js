1c046ec7b054205c25394447d1aaf6c5659855e1
enahoroenas <orieroenahoro@yahoo.com>
2022-11-06 14:38:03 -0800

    added projects

---
+++ src/assets/wrappers/Projectitem.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    background: var(--white);
+    border-radius: var(--borderRadius);
+  //display: grid;
+  //grid-template-rows: 1fr auto;
+    box-shadow: var(--shadow-2);
+
+.container{
+  padding: 20px;
+}
+  .info{
+    margin-top: 20px;
+  }
+  .description{
+    color: #974ede;
+  }
+
+  .amt{
+    font-weight: bolder;
+  }
+
+  .main-icon {
+    width: fit-content;
+    height: 60px;
+    display: grid;
+    place-items: center;
+    background: #5D3FD3;
+    border-radius: 12px;
+    font-size: 1.5rem;
+    font-weight: 700;
+    text-transform: uppercase;
+    color: var(--white);
+    margin-right: 2rem;
+    padding: 10px;
+  }
+
+  @media (min-width: 992px) {
+    .container{
+    margin-left:50px;
+  } 
+  }
+`
+export default Wrapper
