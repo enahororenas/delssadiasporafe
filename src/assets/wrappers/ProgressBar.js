134fed3cf926228e9504886e7a96d89539ea856c
enahoroenas <orieroenahoro@yahoo.com>
2022-11-06 14:37:58 -0800

    added projects

---
+++ src/assets/wrappers/ProgressBar.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.section`
+    .containerStyles{
+    height: 20;
+    width: 100%;
+    background-color: #CBC3E3;
+    border-radius: 12px;
+    margin: 50;
+    
+  }
+
+.total{
+  float:right;
+  font-weight: bold;
+color: #6B4AD4;
+}
+
+  .labelStyles{
+    padding: 5;
+    color:white;
+    font-weight: bold;
+  }
+`
+export default Wrapper
