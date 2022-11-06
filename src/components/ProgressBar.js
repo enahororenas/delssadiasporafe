a3688a3f162fc7c4f44357b5b6c22d0404fcf928
enahoroenas <orieroenahoro@yahoo.com>
2022-11-06 14:38:26 -0800

    added projects

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,35 @@
+import React,{useState,useEffect} from 'react'
+import Wrapper from '../assets/wrappers/ProgressBar'
+
+
+const ProgressBar = ({ completed,total }) => {
+    const [value, setValue] = useState(0);
+
+    useEffect(() => {
+      setValue(Math.round((completed/total)*100));
+    },[completed,total])
+
+    const fillerStyles = {
+        height: '100%',
+        width: `${value}%`,
+        backgroundColor: '#6B4AD4',
+        transition: 'width 1s ease-in-out',
+        borderRadius: 'inherit',
+        textAlign: 'right',
+      }
+
+  return (
+    <Wrapper>
+     <div style={{marginBottom:'20px'}}>
+    <div className='containerStyles'>
+    <div style={fillerStyles}>
+      <span className='labelStyles'>{`${value}%`}</span>
+    </div>
+  </div>
+  <div className='total'>100%</div>
+  </div>   
+  </Wrapper>
+  )
+}
+
+export default ProgressBar
