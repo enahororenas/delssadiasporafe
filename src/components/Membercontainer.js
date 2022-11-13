6569f290606046639abc0a2f447e0b49e481f1ab
enahoroenas <orieroenahoro@yahoo.com>
2022-11-13 03:21:42 -0800

    changed members

--- src/components/Membercontainer.js
+++ src/components/Membercontainer.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/Membercontainer'
-import JobInfo from './JobInfo'
 import { FaLocationArrow, FaBriefcase,FaHouseUser,FaCalendar } from 'react-icons/fa'
 import {MdEmail} from 'react-icons/md'
 import { Fade } from "react-awesome-reveal";
@@ -21,42 +20,18 @@ const Membercontainer = ({fname,lname,email,image,location,yog,occupation,house}
             <h3 className="username">{name}</h3>
            <div className='remain'>
                 <div className='details'>
-                    <p className='text'><MdEmail />&emsp;{email}</p>
-                    
+                    <p className='text'><MdEmail />&emsp;{email}</p>           
                     <p className='text'><FaCalendar/>&emsp;{yog}</p>
                     <p className='text'><FaLocationArrow />&emsp;{location}</p>
                     <p className='text'><FaHouseUser />&emsp; {house}</p>
-                    <p className='text'><FaBriefcase />&emsp;{occupation}</p>
-
-                    {/*<JobInfo icon={<FaBriefcase />} text={occupation} />*/}
-                    
+                    <p className='text'><FaBriefcase />&emsp;{occupation}</p>          
             </div>
             </div> 
         </div>        
-        
                 
             </div>
         </Fade>
       </div>
-
-
-    {/*    <div className="content">
-            <div className="card">
-                <div className="firstinfo"><img src={image} alt='...'/>
-                    <div className="profileinfo">
-                        <h1>{name}</h1>
-                        
-                        <div className='profiledetails'>
-                        <JobInfo icon={<FaCalendar />} text={yog} />
-                        <JobInfo icon={<FaLocationArrow />} text={location} />
-                        <JobInfo icon={<FaHouseUser />} text={house} />
-                        <JobInfo icon={<FaBriefcase />} text={occupation} />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-  */}
         </Wrapper>    
         )
     }
