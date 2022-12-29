d63614a379747690d4c89d6cd9bcb9186d34cea7
enahoroenas <orieroenahoro@yahoo.com>
2022-12-28 20:17:04 -0800

    mod country

--- src/components/Membercontainer.js
+++ src/components/Membercontainer.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/Membercontainer'
 import { FaLocationArrow, FaBriefcase,FaHouseUser,FaCalendar } from 'react-icons/fa'
-import {MdEmail} from 'react-icons/md'
+import {MdEmail,MdOutlineMyLocation} from 'react-icons/md'
 import { Fade } from "react-awesome-reveal";
 
-const Membercontainer = ({fname,lname,email,image,location,yog,occupation,house}) => {
+
+const Membercontainer = ({fname,lname,email,image,location,yog,occupation,house,country}) => {
   const name = fname + ' ' + lname  
   return (
     <Wrapper>
@@ -23,6 +24,7 @@ const Membercontainer = ({fname,lname,email,image,location,yog,occupation,house}
                     <p className='text'><MdEmail />&emsp;{email}</p>           
                     <p className='text'><FaCalendar/>&emsp;{yog}</p>
                     <p className='text'><FaLocationArrow />&emsp;{location}</p>
+                    <p className='text'><MdOutlineMyLocation />&emsp;{country}</p>
                     <p className='text'><FaHouseUser />&emsp; {house}</p>
                     <p className='text'><FaBriefcase />&emsp;{occupation}</p>          
             </div>
